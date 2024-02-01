d1a729b67c4f98e0b127e255fcf1fd6c64d1204b
divyanshu3725 <divyanshu3725@gmail.com>
2024-02-01 09:53:55 +0530

    restart

--- episode4/script.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import  ReactDOM from "react-dom/client";
-const NewElement = () => <h1>This is new element</h1>;
-const rootElement = ReactDOM.createRoot(document.getElementById('root'));
-rootElement.render(<NewElement/>);
